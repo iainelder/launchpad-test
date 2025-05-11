07cb89f7b028fe89322a89577b32b8393c64c2e3
Iain Samuel McLean Elder <iain@isme.es>
2025-05-11 22:32:10 +0200

    Initial commit of dh_make with some ex file removed

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/aws-gate/issues
+# Bug-Submit: https://github.com/<user>/aws-gate/issues/new
+# Changelog: https://github.com/<user>/aws-gate/blob/master/CHANGES
+# Documentation: https://github.com/<user>/aws-gate/wiki
+# Repository-Browse: https://github.com/<user>/aws-gate
+# Repository: https://github.com/<user>/aws-gate.git
